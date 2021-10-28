57a55b3bc3c34b28477d532d27fe11cbce57a4a3
gk mamun <gkmamun26@gmail.com>
2021-10-29 04:03:35 +0800

    first section

---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post({ title, description, upvotes, postedBy, userImage }) {
+    return (
+        <div>
+            post
+        </div>
+    )
+}
+
+export default Post
